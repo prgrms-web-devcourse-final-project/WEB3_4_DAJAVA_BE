e160ffddb4079fda99472bb4f4be168c465e09d5
ChoiHyunSan <chsan626@gmail.com>
2025-03-24 11:11:43 +0900

    feat: 솔루션 신청관련 엔티티생성

---
+++ backend/src/main/java/com/dajava/backend/domain/solution/entity/Order.java
@@ -0,0 +1,31 @@
+package com.dajava.backend.domain.solution.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String url;
+}
---
+++ backend/src/main/java/com/dajava/backend/domain/solution/entity/Solution.java
@@ -0,0 +1,78 @@
+package com.dajava.backend.domain.solution.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.dajava.backend.domain.solution.dto.SolutionCreateRequest;
+import com.dajava.backend.global.common.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Solution extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String serialNumber;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String url;
+
+	@Column(nullable = false)
+	private LocalDateTime startDate;
+
+	@Column(nullable = false)
+	private LocalDateTime endDate;
+
+	@Column(nullable = false)
+	private int duration;
+
+	@Column(nullable = false)
+	private boolean isServiceExpired;
+
+	@Column(nullable = false)
+	private boolean isSolutionComplete;
+
+	public static Solution create(
+		final SolutionCreateRequest request,
+		final int duration
+	) {
+		return Solution.builder()
+			.serialNumber(createSerialNumber())
+			.email(request.email())
+			.password(request.password())
+			.url(request.url())
+			.startDate(request.startDate())
+			.endDate(request.endDate())
+			.duration(duration)
+			.isServiceExpired(false)
+			.isSolutionComplete(false)
+			.build();
+	}
+
+	private static String createSerialNumber() {
+		return UUID.randomUUID().toString();
+	}
+}
