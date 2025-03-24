7716d527a68e26af7879dce60c5f28933aee2ccc
Metronon <bumpercar07@gmail.com>
2025-03-24 16:02:31 +0900

    refactor: sessionDataService Key 사용으로 최적화

--- backend/src/main/java/com/dajava/backend/domain/event/service/SessionDataService.java
+++ backend/src/main/java/com/dajava/backend/domain/event/service/SessionDataService.java
@@ -7,24 +7,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dajava.backend.domain.event.SessionData;
+import com.dajava.backend.domain.event.dto.SessionDataKey;
 import com.dajava.backend.domain.event.repository.SessionDataRepository;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * SessionData 가 현재 Cache 에 존재하는지 확인하고, 없을 시 생성 및 Cache 에 저장하는 로직입니다.
+ * computeIfAbsent 로직을 통해 Cache 에 데이터가 존재하지 않을시 생성 및 Cache 에 올려놓습니다.
+ *
+ * @author Metronon
+ */
 @Service
 @RequiredArgsConstructor
 public class SessionDataService {
 	private final SessionDataRepository sessionDataRepository;
 
-	private final Map<String, SessionData> sessionCache = new ConcurrentHashMap<>();
+	private final Map<SessionDataKey, SessionData> sessionCache = new ConcurrentHashMap<>();
 
 	@Transactional
 	public SessionData createOrFindSessionData(String pageUrl, String sessionId, String memberSerialNumber) {
-		SessionData sessionData = sessionCache.get(pageUrl + sessionId + memberSerialNumber);
+		SessionDataKey key = new SessionDataKey(sessionId, pageUrl, memberSerialNumber);
 
-		if (sessionData == null) {
-			sessionData = sessionDataRepository.findByPageUrlAndSessionIdAndMemberSerialNumber(pageUrl, sessionId,
-					memberSerialNumber)
+		return sessionCache.computeIfAbsent(key, k ->
+			sessionDataRepository.findByPageUrlAndSessionIdAndMemberSerialNumber(pageUrl, sessionId, memberSerialNumber)
 				.orElseGet(() -> {
 					SessionData newSession = SessionData.builder()
 						.pageUrl(pageUrl)
@@ -35,9 +41,7 @@ public class SessionDataService {
 						.isVerified(false)
 						.build();
 					return sessionDataRepository.save(newSession);
-				});
-			sessionCache.put(pageUrl + sessionId + memberSerialNumber, sessionData);
-		}
-		return sessionData;
+				})
+		);
 	}
 }
