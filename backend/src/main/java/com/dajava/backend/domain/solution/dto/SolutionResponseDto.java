ceba95b272128227dd1097a575374c8aa7780723
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-01 17:28:44 +0900

    refactor: 검증 실패 시 반환되는 에러 메시지추가, @Lob 어노테이션 text에 추가

--- backend/src/main/java/com/dajava/backend/domain/solution/dto/SolutionResponseDto.java
+++ backend/src/main/java/com/dajava/backend/domain/solution/dto/SolutionResponseDto.java
@@ -1,8 +1,15 @@
 package com.dajava.backend.domain.solution.dto;
 
+import jakarta.persistence.Lob;
 import jakarta.validation.constraints.NotBlank;
 
+
 public record SolutionResponseDto (
-	@NotBlank String registerSerialNumber,
-	@NotBlank  String text
+	@NotBlank(message = "등록 serial 번호는 비어 있을 수 없습니다.")
+	String registerSerialNumber,
+
+	@Lob
+	@NotBlank(message = "솔루션 내용은 비어 있을 수 없습니다.")
+	String text
+
 ) {}
