f40f243c72f8c907fd06b05c8abf6d73d7c275ac
ChoiHyunSan <chsan626@gmail.com>
2025-04-05 15:44:37 +0900

    feat : 솔루션 조회 API Request 양식 변경

--- backend/src/main/java/com/dajava/backend/domain/register/controller/RegisterController.java
+++ backend/src/main/java/com/dajava/backend/domain/register/controller/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -119,7 +120,7 @@ public class RegisterController {
 	@GetMapping("/v1/registers")
 	@ResponseStatus(HttpStatus.OK)
 	public RegistersInfoResponse list(
-		@RequestBody RegistersInfoRequest request,
+		@ModelAttribute RegistersInfoRequest request,
 		HttpServletRequest httpRequest
 	) {
 		adminService.authorize(httpRequest);
