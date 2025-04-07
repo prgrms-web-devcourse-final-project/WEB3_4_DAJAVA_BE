414236a51fab4025a011f3f07423fa3e4bb0fb5a
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-07 16:33:02 +0900

    feat: redis config

--- backend/src/main/java/com/dajava/backend/redis/config/RedisConfig.java
+++ backend/src/main/java/com/dajava/backend/redis/config/RedisConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,16 +16,5 @@ public class RedisConfig {
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(host, port);
-	}
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-		// Redis와 연결할 수 있도록 커넥션 팩토리를 설정
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(connectionFactory);
-		// key는 문자열
-		template.setKeySerializer(new StringRedisSerializer());
-		// Object 형태니까 JSON 형식으로 직렬화/역직렬화
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-		return template;
 	}
 }
