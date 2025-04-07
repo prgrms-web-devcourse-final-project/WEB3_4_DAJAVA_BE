3804c4b761941bc8676a2c12ce3a6a74681bdea9
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-07 10:43:47 +0900

    fix: rediscache 삭제

--- backend/src/main/java/com/dajava/backend/domain/event/repository/PointerEventRepository.java
+++ backend/src/main/java/com/dajava/backend/domain/event/repository/PointerEventRepository.java
@@ -3,6 +3,3 @@ package com.dajava.backend.domain.event.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dajava.backend.domain.event.entity.SolutionEvent;
-
-public interface PointerEventRepository extends JpaRepository<SolutionEvent, Long> {
-}
