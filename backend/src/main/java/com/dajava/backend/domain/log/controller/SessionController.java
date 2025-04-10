c383a657d6385242fd3ace6d8b357cae10285757
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-10 22:52:22 +0900

    fix: start api 삭제

--- backend/src/main/java/com/dajava/backend/domain/log/controller/SessionController.java
+++ backend/src/main/java/com/dajava/backend/domain/log/controller/SessionController.java
@@ -3,13 +3,11 @@ package com.dajava.backend.domain.log.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dajava.backend.domain.log.service.SessionService;
-import com.dajava.backend.domain.log.dto.identifier.SessionIdentifier;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,13 +19,6 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "SessionController", description = "세션 컨트롤러")
 public class SessionController {
 	final SessionService sessionService;
-
-	@Operation(summary = "세션 시작 요청", description = "세션 시작 요청이 들어오면 해당 세션을 시작합니다.")
-	@PostMapping("/start")
-	@ResponseStatus(HttpStatus.OK)
-	public void logStart(@RequestBody SessionIdentifier sessionIdentifier) {
-		sessionService.startSession(sessionIdentifier);
-	}
 
 	@Operation(summary = "세션 종료 요청", description = "세션 종료 요청이 들어오면 해당 세션을 종료합니다.")
 	@PostMapping("/end/{sessionId}")
