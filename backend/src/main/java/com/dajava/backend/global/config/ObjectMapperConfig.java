02bda5ddaa41d1d3a6c89992e819e7b4a41568f4
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-04-11 00:04:23 +0900

    fix: Timestamp 관련 파싱 오류 해결

--- backend/src/main/java/com/dajava/backend/global/config/ObjectMapperConfig.java
+++ backend/src/main/java/com/dajava/backend/global/config/ObjectMapperConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class ObjectMapperConfig {
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+		// Java 8 날짜/시간 처리 모듈 등록
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
 	}
 }
