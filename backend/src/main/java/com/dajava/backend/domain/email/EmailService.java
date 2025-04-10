da638bf3282bfeaf20746dbb0e56b624190cbd33
Metronon <bumpercar07@gmail.com>
2025-04-10 10:47:04 +0900

    feat: email config 추가 및 오류 테스트 수정

--- backend/src/main/java/com/dajava/backend/domain/email/EmailService.java
+++ backend/src/main/java/com/dajava/backend/domain/email/EmailService.java
@@ -1,4 +1,26 @@
 package com.dajava.backend.domain.email;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+
+	private final JavaMailSender mailSender;
+
+	public EmailService(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	@Async
+	public void sendEmail(String to, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		message.setFrom("noreply@dajava.com");
+		mailSender.send(message);
+	}
 }
