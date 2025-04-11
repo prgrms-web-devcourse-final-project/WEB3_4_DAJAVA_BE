ab6307eb0421f6b9f94a93ec8edae8f1743e41fa
ChoiHyunSan <chsan626@gmail.com>
2025-04-11 14:38:56 +0900

    bugfix : RegisterExpireScheduler에서 만료시키는 로직을 트랜잭션으로 처리

--- backend/src/main/java/com/dajava/backend/domain/register/scheduler/RegisterExpireScheduler.java
+++ backend/src/main/java/com/dajava/backend/domain/register/scheduler/RegisterExpireScheduler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dajava.backend.domain.register.entity.Register;
 import com.dajava.backend.domain.register.repository.RegisterRepository;
@@ -21,8 +22,8 @@ public class RegisterExpireScheduler {
 	private final RegisterRepository registerRepository;
 
 	@Scheduled(cron = "0 0 0 * * *")
+	@Transactional
 	public void registerExpire() {
-
 		// 만료 대상 Register를 조회하여 처리한다.
 		registerRepository.findExpiredTarget(LocalDateTime.now()).forEach(Register::expire);
 	}
