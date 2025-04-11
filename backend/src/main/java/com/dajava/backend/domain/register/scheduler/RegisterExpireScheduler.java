8d683aca69b4e684ed2189f2a6b8b7024dd0c5de
ChoiHyunSan <chsan626@gmail.com>
2025-04-11 14:29:15 +0900

    feat : 만료 대상 Register를 만료처리하도록 스케줄러 기능 추가

---
+++ backend/src/main/java/com/dajava/backend/domain/register/scheduler/RegisterExpireScheduler.java
@@ -0,0 +1,29 @@
+package com.dajava.backend.domain.register.scheduler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.dajava.backend.domain.register.entity.Register;
+import com.dajava.backend.domain.register.repository.RegisterRepository;
+import com.dajava.backend.domain.register.service.RegisterService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RegisterExpireScheduler {
+
+	private final RegisterRepository registerRepository;
+
+	@Scheduled(cron = "0 0 0 * * *")
+	public void registerExpire() {
+
+		// 만료 대상 Register를 조회하여 처리한다.
+		registerRepository.findExpiredTarget(LocalDateTime.now()).forEach(Register::expire);
+	}
+}
